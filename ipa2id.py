695df9afd37d97b6b6e88ee630621fc9188d0d83
Jack <31581463+oganessium@users.noreply.github.com>
2018-09-08 12:33:33 -0400

    added all in folder support
    also added filename to results, removed system pauses in favor of all platforms

--- ipa2id.py
+++ ipa2id.py
@@ -1,53 +1,63 @@
 try:
-    import plistlib, zipfile, os
-    from tkinter.filedialog import askopenfilename
+    import plistlib, zipfile, os, ntpath
+    from tkinter.filedialog import askopenfilename, askdirectory
     from tkinter import Tk
 except:
-    print("This tool requires tkinter, plistlib, os and zipfile modules to function correctly!")
+    print("This tool requires tkinter, ntpath, plistlib, os and zipfile modules to function correctly!")
 def new_ipa():
 
     Tk().withdraw() # Hides tkinter window
-
-    fil = askopenfilename(title="Select an IPA archive", filetypes = (("iPhone Application Archive","*.ipa"),("Compressed folder", "*.zip"),("all files","*.*")))
+    fil = []
+    ask = input("Please input 'i'/'ipa' for single ipa or 'a'/'all' for all in folder: ")
+    if ask.lower() == "i" or ask.lower()=="ipa":
+        fil.append(askopenfilename(title="Select an IPA archive", filetypes = (("iPhone Application Archive","*.ipa"),("Compressed folder", "*.zip"),("all files","*.*"))))
+    elif ask.lower() == "a" or ask.lower()=="all":
+        batch = askdirectory(title="Select a folder")
+        for fname in [os.path.join(batch, f) for f in os.listdir(batch) if os.path.isfile(os.path.join(batch, f)) and f.endswith(".ipa")]:
+            fil.append(fname)
+    else:
+        print("Invalid option!")
+        exit()
     # shows file open
 
-    try:
-        zpf = zipfile.ZipFile(fil)
-        # zip object
-    except:
-        print("An error occurred in importing the file as a ZIP Archive!")
-        os.system('pause')
-        exit()
+    for item in fil:
+        try:
+            zpf = zipfile.ZipFile(item)
+            # zip object
+        except:
+            print("An error occurred in importing the file as a ZIP Archive!")
+            continue
 
-    try:
-        namespace = [item for item in zpf.namelist() if item.lower().endswith(".app/info.plist")][0]
-        # gets path of info.plist
-    except:
-        print("An error occurred when trying to find the IPA information!")
-        os.system('pause')
-        exit()
+        try:
+            namespace = [item for item in zpf.namelist() if item.lower().endswith(".app/info.plist")][0]
+            # gets path of info.plist
+        except:
+            print("An error occurred when trying to find the IPA information!")
+            continue
 
-    info = zpf.open(namespace) # opens binary plist
+        info = zpf.open(namespace) # opens binary plist
 
-    info_r = info.read() # reads plist data
+        info_r = info.read() # reads plist data
 
-    try:
-        final_id = plistlib.loads(info_r) # loads binary as dictionary
-    except:
-        print("An error occurred when trying to decode the bundle information!")
-        os.system('pause')
-        exit()
+        try:
+            final_id = plistlib.loads(info_r) # loads binary as dictionary
+        except:
+            print("An error occurred when trying to decode the bundle information!")
+            continue
+
+        print("--------------------------------------------")
+        try:
+            print("{} ({})".format(final_id['CFBundleDisplayName'],ntpath.basename(item))) # display name
+        except:
+            try:
+                print("{} ({})".format(final_id['CFBundleExecutable'], ntpath.basename(item)))
+            except Exception as e:
+                print("Unknown error")
+                print(e)
+                print(item)
+        print(final_id['CFBundleIdentifier']) # bundle id
 
     print("--------------------------------------------")
-    try:
-        print(final_id['CFBundleDisplayName']) # display name
-    except:
-        try:
-            print(final_id['CFBundleExecutable'])
-        except:
-            print(fil)
-    print(final_id['CFBundleIdentifier']) # bundle id
-
     yn = input("Find another Bundle ID? (Y/N) ")
     if yn.lower()=="y":
         new_ipa()
